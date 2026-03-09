8352e44a93e97c6fe882e3d228319afe4cb5c6a1
Andrii-Osipov <aosipov@anaconda.com>
2026-03-09 08:08:23 -0700

    libutf8proc 2.11.3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,8 +8,12 @@ set BUILD_TYPE="Release"
 cmake .. -G "%CMAKE_GENERATOR%" ^
   -DBUILD_SHARED_LIBS=ON ^
   -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" ^
-  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+  -DUTF8PROC_ENABLE_TESTING=ON
 if errorlevel 1 exit \b 1
 
 cmake --build . --config %BUILD_TYPE% --target install
+if errorlevel 1 exit \b 1
+
+ctest --output-on-failure
 if errorlevel 1 exit \b 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,9 @@ export BUILD_TYPE="Release"
 cmake ${CMAKE_ARGS} .. -G "${CMAKE_GENERATOR}" \
   -DBUILD_SHARED_LIBS=ON \
   -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DUTF8PROC_ENABLE_TESTING=ON
 
 cmake --build . --config "${BUILD_TYPE}" --target install
+
+ctest --output-on-failure
